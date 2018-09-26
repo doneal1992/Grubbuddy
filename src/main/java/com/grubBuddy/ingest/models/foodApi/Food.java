fbe79abbcfdacaffc702f12b21af166d0981ee7f
Danny ONeal <danny.oneal@synthiocorp.com>
2018-09-25 23:41:08 -0400

    Created json serializer since api responses are same structure. cleaned up reactive code.

---
+++ src/main/java/com/grubBuddy/ingest/models/foodApi/Food.java
@@ -0,0 +1,83 @@
+package com.grubBuddy.ingest.models.foodApi;
+
+import com.fasterxml.jackson.annotation.JsonAnyGetter;
+import com.fasterxml.jackson.annotation.JsonAnySetter;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Document(collection = "foods")
+@JsonDeserialize(as = Food.class)
+public class Food implements FoodItem {
+    @JsonProperty("offset")
+    private Integer offset;
+
+    @Id
+    @JsonProperty("id")
+    private String id;
+    @JsonProperty("name")
+    private String name;
+
+    @JsonIgnore
+    private Map<String, Object> additionalProperties = new HashMap<String, Object>();
+    private final static long serialVersionUID = 8169660668666539826L;
+
+    public FoodItem withOffset(Integer offset) {
+        this.offset = offset;
+        return this;
+    }
+
+    public FoodItem withId(String id) {
+        this.id = id;
+        return this;
+    }
+
+    public FoodItem withName(String name) {
+        this.name = name;
+        return this;
+    }
+
+    @JsonAnyGetter
+    public Map<String, Object> getAdditionalProperties() {
+        return this.additionalProperties;
+    }
+
+    @JsonAnySetter
+    public void setAdditionalProperty(String name, Object value) {
+        this.additionalProperties.put(name, value);
+    }
+
+    public FoodItem withAdditionalProperty(String name, Object value) {
+        this.additionalProperties.put(name, value);
+        return this;
+    }
+
+    public Integer getOffset() {
+        return offset;
+    }
+
+    public void setOffset(Integer offset) {
+        this.offset = offset;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
--- src/main/java/com/grubBuddy/ingest/service/FoodService.java
+++ src/main/java/com/grubBuddy/ingest/service/FoodService.java
@@ -1,16 +1,24 @@
 package com.grubBuddy.ingest.service;
 
-import com.grubBuddy.ingest.api.FoodListApi;
+import com.grubBuddy.ingest.api.FoodCompositionApi;
 import com.grubBuddy.ingest.dao.FoodDAO;
+import com.grubBuddy.ingest.models.foodApi.FoodApiResponse;
+import com.grubBuddy.ingest.models.foodApi.FoodItem;
+import com.grubBuddy.ingest.models.foodApi.FoodList;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import reactor.core.publisher.Mono;
 import reactor.core.scheduler.Schedulers;
+import reactor.util.function.Tuple2;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Component
 public class FoodService {
 
     @Autowired
-    private FoodListApi foodListApi;
+    private FoodCompositionApi foodCompositionApi;
 
     @Autowired
     private FoodDAO foodDAO;
@@ -25,23 +33,55 @@ public class FoodService {
         while(!isDone[0]) {
             if(inProcess[0] != 5) {
                 inProcess[0] = inProcess[0] + 1;
-                this.foodDAO.insertMany(this.foodListApi.getFoods(take, page + 1)
-                    .map(x -> {
-                        if(x == null) {
-                            isDone[0] = true;
-                            return null;
-                        };
-                        return x.list.foodItems;
-                    }))
-                    .doOnComplete(() -> inProcess[0] = inProcess[0] - 1)
-                    .doOnError(err -> isDone[0] = true)
-                    .subscribeOn(Schedulers.parallel())
-                    .subscribe();
-
+                syncFoodAndNutrients(page, take, isDone, inProcess);
                 page += take;
             }
+
             Thread.sleep(0, 1);
         }
+    }
+
+    private void syncFoodAndNutrients(int page, int take, boolean[] isDone, int[] inProcess) {
+        Mono<Tuple2<FoodApiResponse, FoodApiResponse>> dataSync = foodItemSync(page, take);
+
+        this.foodDAO.insertMany(
+                dataSync.map(x -> mapFoodItems(isDone, x))
+            )
+            .doOnComplete(() -> inProcess[0] = inProcess[0] - 1)
+            .doOnError(err ->  {
+                System.out.println(err);
+                isDone[0] = true;
+            })
+            .subscribeOn(Schedulers.parallel())
+            .subscribe();
+    }
+
+    private Mono<Tuple2<FoodApiResponse, FoodApiResponse>> foodItemSync(int page, int take) {
+        Mono<FoodApiResponse> syncFoodItems = getFoods(page, take);
+        Mono<FoodApiResponse> syncNutrients = getNutrients(page, take);
+
+        return syncFoodItems.zipWith(syncNutrients);
+    }
+
+    private Mono<FoodApiResponse> getFoods(int page, int take) {
+        return this.foodCompositionApi.getFoodItems(take, page + 1, "f")
+                    .defaultIfEmpty(new FoodApiResponse().withList(new FoodList().withItem(new ArrayList<>())));
+    }
+
+    private Mono<FoodApiResponse> getNutrients(int page, int take) {
+        return this.foodCompositionApi.getFoodItems(take, page + 1, "n")
+                    .defaultIfEmpty(new FoodApiResponse().withList(new FoodList().withItem(new ArrayList<>())));
+    }
+
+    private List<FoodItem> mapFoodItems(boolean[] isDone, Tuple2<FoodApiResponse, FoodApiResponse> x) {
+        if(x == null) {
+            isDone[0] = true;
+            return null;
+        }
+        List<FoodItem> items = new ArrayList<>();
+        items.addAll(x.getT1().list.foodItems);
+        items.addAll(x.getT2().list.foodItems);
+        return  items;
     }
 
 }
